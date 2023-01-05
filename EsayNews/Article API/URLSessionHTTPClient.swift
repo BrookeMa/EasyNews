ab1c7a488abeb37f475a25e313d51430bd527b1e
Brook MK <maye634028187@icloud.com>
2023-01-05 15:42:00 +0000

    Make `URLSessionHTTPClient` conform the `HTTPClient` protocol

---
+++ EsayNews/Article API/URLSessionHTTPClient.swift
@@ -0,0 +1,33 @@
+//
+//  URLSessionHTTPClient.swift
+//  EsayNews
+//
+//  Created by Ye Ma on 05/01/2023.
+//
+
+import Foundation
+
+public final class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    private struct UnexpectedValuesRepresentation: Error {}
+    
+    public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            completion(Result {
+                if let error = error {
+                    throw error
+                } else if let data = data, let response = response as? HTTPURLResponse {
+                    return (data, response)
+                } else {
+                    throw UnexpectedValuesRepresentation()
+                }
+            })
+        }.resume()
+    }
+}
+
