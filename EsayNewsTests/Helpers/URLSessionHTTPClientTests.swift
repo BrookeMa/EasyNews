5af57e618b52c0cfceb31090c95c103018a799ba
Brook MK <maye634028187@icloud.com>
2023-01-05 16:38:06 +0000

    Move `URLSessionHTTPClientTests` to it own file

---
+++ EsayNewsTests/Helpers/URLSessionHTTPClientTests.swift
@@ -0,0 +1,31 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  EsayNewsTests
+//
+//  Created by Ye Ma on 05/01/2023.
+//
+
+import Foundation
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
+
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
+
+func anyData() -> Data {
+    return Data("any data".utf8)
+}
+
+func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+    let json = ["data": items]
+    return try! JSONSerialization.data(withJSONObject: json)
+}
+
+extension HTTPURLResponse {
+    convenience init(statusCode: Int) {
+        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+    }
+}
