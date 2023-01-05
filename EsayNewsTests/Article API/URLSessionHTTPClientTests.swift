897020561bf4ec8dc3c7f2019467789a1c8dad3f
Brook MK <maye634028187@icloud.com>
2023-01-05 17:29:42 +0000

    Guarantees correct URL and HTTP method on get requests

--- EsayNewsTests/Article API/URLSessionHTTPClientTests.swift
+++ EsayNewsTests/Article API/URLSessionHTTPClientTests.swift
@@ -12,10 +12,28 @@ final class URLSessionHTTPClientTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
+        
+        URLProtocolStub.startInterceptingRequests()
     }
     
     override func tearDown() {
         super.tearDown()
+        URLProtocolStub.stopInterceptionRequests()
+    }
+    
+    func test_getFromURL_performsGETRequestWithURL() {
+        let url = anyURL()
+        let exp = expectation(description: "Wait for request")
+        
+        URLProtocolStub.observeRequests { request in
+            XCTAssertEqual(request.url, url)
+            XCTAssertEqual(request.httpMethod, "GET")
+            exp.fulfill()
+        }
+        
+        makeSUT().get(from: url) { _ in }
+        
+        wait(for: [exp], timeout: 1.0)
     }
     
     // MARK: - Helpers
