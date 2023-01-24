f1ea7db7413293594e9a6f0fe066a8861c84d7a5
Brook MK <maye634028187@icloud.com>
2023-01-24 16:18:25 +0000

    Add CoreDataArticleStore data model

---
+++ EsayNews/Article Cache/ManagedArticle.swift
@@ -0,0 +1,20 @@
+//
+//  ManagedArticle.swift
+//  EsayNews
+//
+//  Created by Ye Ma on 24/01/2023.
+//
+
+import CoreData
+
+@objc(ManagedArticle)
+class ManagedArticle: NSManagedObject {
+    @NSManaged var author: String?
+    @NSManaged var title: String
+    @NSManaged var desc: String
+    @NSManaged var url: URL
+    @NSManaged var source: String
+    @NSManaged var image: URL?
+    @NSManaged var published_at: Date
+}
+
