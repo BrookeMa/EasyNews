9298d5d8d50420b7b7c795f0d12f4263ea109ffb
Brook MK <maye634028187@icloud.com>
2023-01-27 14:47:47 +0000

    Add `ArticleStore` protocol to achieve decoupling from the database

---
+++ EsayNews/Article Cache/Infrastructure/ArticleStore.swift
@@ -0,0 +1,33 @@
+//
+//  ArticleStore.swift
+//  EsayNews
+//
+//  Created by Ye Ma on 26/01/2023.
+//
+
+import Foundation
+
+public typealias CachedArticle = (articles: [LocalArticle], timestamp: Date)
+
+public protocol ArticleStore {
+    typealias DeletionResult = Result<Void, Error>
+    typealias DeletionCompletion = (DeletionResult) -> Void
+    
+    typealias InsertionResult = Result<Void, Error>
+    typealias InsertionCompletion = (InsertionResult) -> Void
+    
+    typealias RetrievalResult = Result<CachedArticle?, Error>
+    typealias RetrievalCompletion = (RetrievalResult) -> Void
+
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
+    func deleteCachedArticle(completion: @escaping DeletionCompletion)
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
+    func insert(_ articles: [LocalArticle], timestamp: Date, completion: @escaping InsertionCompletion)
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
+    func retrieve(completion: @escaping RetrievalCompletion)
+}
---
+++ EsayNewsTests/Feed Cache/Helpers/ArticleStoreSpy.swift
@@ -0,0 +1,62 @@
+//
+//  ArticleStoreSpy.swift
+//  EsayNewsTests
+//
+//  Created by Ye Ma on 26/01/2023.
+//
+
+import Foundation
+import EsayNews
+
+class ArticleStoreSpy: ArticleStore {
+    enum ReceivedMessage: Equatable {
+        case deleteCachedArticle
+        case insert([LocalArticle], Date)
+        case retrieve
+    }
+    
+    private(set) var receivedMessages = [ReceivedMessage]()
+    
+    private var deletionCompletions = [DeletionCompletion]()
+    private var insertionCompletions = [InsertionCompletion]()
+    private var retrievalCompletions = [RetrievalCompletion]()
+    
+    func deleteCachedArticle(completion: @escaping DeletionCompletion) {
+        deletionCompletions.append(completion)
+        receivedMessages.append(.deleteCachedArticle)
+    }
+    
+    func completeDeletion(with error: Error, at index: Int = 0) {
+        deletionCompletions[index](.failure(error))
+    }
+    
+    func completeDeletionSuccessfully(at index: Int = 0) {
+        deletionCompletions[index](.success(()))
+    }
+    
+    func insert(_ articles: [EsayNews.LocalArticle], timestamp: Date, completion: @escaping InsertionCompletion) {
+        insertionCompletions.append(completion)
+        receivedMessages.append(.insert(articles, timestamp))
+    }
+    
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](.failure(error))
+    }
+    
+    func retrieve(completion: @escaping RetrievalCompletion) {
+        retrievalCompletions.append(completion)
+        receivedMessages.append(.retrieve)
+    }
+    
+    func completeRetrieval(with error: Error, at index: Int = 0) {
+        retrievalCompletions[index](.failure(error))
+    }
+    
+    func completeRetrievalWithEmptyCache(at index: Int = 0) {
+        retrievalCompletions[index](.success(.none))
+    }
+    
+    func completeRetrieval(with articles: [LocalArticle], timestamp: Date, at index: Int = 0) {
+        retrievalCompletions[index](.success(CachedArticle(articles: articles, timestamp: timestamp)))
+    }
+}
