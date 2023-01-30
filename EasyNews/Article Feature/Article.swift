504c70f574b67f24ce45f947320ab5472d841a45
Brook MK <maye634028187@icloud.com>
2023-01-30 14:37:35 +0000

    Make the `Article` parameters public

--- EasyNews/Article Feature/Article.swift
+++ EasyNews/Article Feature/Article.swift
@@ -7,22 +7,22 @@
 
 import Foundation
 
-public struct Article: Hashable {
-    let author: String?
-    let title: String
-    let description: String
-    let url: URL
-    let source: String
-    let image: URL?
-    let published_at: Date
+public struct Article: Equatable {
+    public let author: String?
+    public let title: String
+    public let description: String
+    public let url: URL
+    public let source: String
+    public let image: URL?
+    public let published: Date
     
-    init(author: String?, title: String, description: String, url: URL, source: String, image: URL?, published_at: Date) {
+    public init(author: String?, title: String, description: String, url: URL, source: String, image: URL?, published: Date) {
         self.author = author
         self.title = title
         self.description = description
         self.url = url
         self.source = source
         self.image = image
-        self.published_at = published_at
+        self.published = published
     }
 }
