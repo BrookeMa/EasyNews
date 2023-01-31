830c59c9d5cf9ffc13738060f0bb3f07e9aa0599
Brook MK <maye634028187@icloud.com>
2023-01-31 21:17:51 +0000

    `LocalArticlesLoader` load articles meanwhile check the time valid

--- EasyNews/Article Cache/Infrastructure/LocalArticlesLoader.swift
+++ EasyNews/Article Cache/Infrastructure/LocalArticlesLoader.swift
@@ -44,6 +44,33 @@ extension LocalArticlesLoader {
     }
 }
 
+extension LocalArticlesLoader: ArticleLoader {
+    public typealias LoadResult = ArticleLoader.Result
+    
+    public func load(completion: @escaping (LoadResult) -> Void) {
+        store.retrieve { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case let .failure(error):
+                completion(.failure(error))
+                
+            case let .success(.some(cache)) where ArticleCachePolicy.validate(cache.timestamp, against: self.currentDate()):
+                completion(.success(cache.articles.toModels()))
+                
+            case .success:
+                completion(.success([]))
+            }
+        }
+    }
+}
+
+private extension Array where Element == LocalArticle {
+    func toModels() -> [Article] {
+        return map { Article(author: $0.author, title: $0.title, description: $0.description, url: $0.url, source: $0.source, image: $0.image, published: $0.published) }
+    }
+}
+
 private extension Array where Element == Article {
     func toLocal() -> [LocalArticle] {
         return map { LocalArticle(author: $0.author, title: $0.title, description: $0.description, url: $0.url, source: $0.source, image: $0.image, published: $0.published) }
