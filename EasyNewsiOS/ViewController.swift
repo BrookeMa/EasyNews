25a1882b294f1f944a5e5d7e0562477585c6c479
Brook MK <maye634028187@icloud.com>
2023-02-02 16:19:03 +0000

    Add `EasyNewsiOS` framwork targets for iOS-plaform-specific components

---
+++ EasyNewsiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyNewsiOS
+//
+//  Created by Ye Ma on 02/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
