219f62fc9fbe9b309934d148abac1da0adfda33c
Brook MK <5072083+BrookeMa@users.noreply.github.com>
2023-02-05 12:54:22 +0000

    Localized `localizable.strings` file

---
+++ EasyNews/Shared Presentation/Localized.swift
@@ -0,0 +1,14 @@
+//
+//  Localized.swift
+//  EasyNews
+//
+//  Created by Ye Ma on 05/02/2023.
+//
+
+import Foundation
+
+final class Localized {
+    static var bundle: Bundle {
+        Bundle(for: Localized.self)
+    }
+}
---
+++ EasyNews/Top Headline UI/Presentation/Localized+TopHeadlineViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  Localized+TopHeadlineViewModel.swift
+//  EasyNews
+//
+//  Created by Ye Ma on 05/02/2023.
+//
+
+import Foundation
+
+extension Localized {
+    enum TopHeadline {
+        static var table: String { "TopHeadline" }
+        
+        static var loadError: String {
+            NSLocalizedString("TOPHEADLINE_VIEW_CONNECTION_ERROR",
+                              tableName: table,
+                              bundle: bundle,
+                              comment: "Error message displayed when we can't load the articles from the server")
+        }
+    }
+}
