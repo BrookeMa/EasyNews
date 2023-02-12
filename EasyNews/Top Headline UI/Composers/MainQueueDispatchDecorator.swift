0749d591491ea798360f7edcc3d943a18cc99594
Brook MK <5072083+BrookeMa@users.noreply.github.com>
2023-02-12 14:46:47 +0000

    Make `MainQueueDispatchDecorator` generic

---
+++ EasyNews/Top Headline UI/Composers/MainQueueDispatchDecorator.swift
@@ -0,0 +1,33 @@
+//
+//  MainQueueDispatchDecorator.swift
+//  EasyNews
+//
+//  Created by Ye Ma on 12/02/2023.
+//
+
+import Foundation
+import EasyNewsFeature
+
+final class MainQueueDispatchDecorator<T> {
+    private let decoratee: T
+
+    init(decoratee: T) {
+        self.decoratee = decoratee
+    }
+
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async(execute: completion)
+        }
+
+        completion()
+    }
+}
+
+extension MainQueueDispatchDecorator: ArticleLoader where T == ArticleLoader {
+    func load(completion: @escaping (ArticleLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+            self?.dispatch { completion(result) }
+        }
+    }
+}
